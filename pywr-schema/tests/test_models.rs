43c3adf9f96f93026b7860a588952d2cfe921051
James Tomlinson <tomo.bbe@gmail.com>
2025-01-07 09:10:00 +0000

    fix: Try to convert RiverSplitWithGauge factors correctly. (#318)
    Add a conversion function for the factors on RiverSplitWithGauge.
    The factors have changed in this implementation and require
    conversion from ratios to proportions. This is only possible
    if the existing factors are all constants.
    
    Fixes #241.

--- pywr-schema/tests/test_models.rs
+++ pywr-schema/tests/test_models.rs
@@ -125,6 +125,7 @@ macro_rules! convert_tests {
 convert_tests! {
     test_convert_timeseries: ("v1/timeseries.json", "v1/timeseries-converted.json"),
     test_convert_inline_parameter: ("v1/inline-parameter.json", "v1/inline-parameter-converted.json"),
+    test_convert_river_split_with_gauge1: ("v1/river_split_with_gauge1.json", "v1/river_split_with_gauge1-converted.json"),
 }
 
 fn convert_model(v1_path: &Path, v2_path: &Path) {
