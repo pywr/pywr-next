8c01c0d10825df9c227b63b4167e3b7add2973a2
James Tomlinson <tomo.bbe@gmail.com>
2026-05-06 13:02:39 +0100

    fix(core): Fix incorrect default lower bound. (#671)
    Three of the solvers were using f64::MIN, the smallest positive
    non-zero number, instead of -f64::MAX the largest negative number.
    This is used as the lower bounds on unbounded rows.
    
    In practice this would only impact inactive virtual nodes.

--- pywr-core/src/solvers/clp/mod.rs
+++ pywr-core/src/solvers/clp/mod.rs
@@ -370,7 +370,7 @@ impl Solver for ClpSolver {
         values: &ConstParameterValues,
         _settings: &Self::Settings,
     ) -> Result<Box<Self>, SolverSetupError> {
-        let builder = SolverBuilder::new(f64::MAX, f64::MIN);
+        let builder = SolverBuilder::new(f64::MAX, -f64::MAX);
         let built = builder.create(model, values)?;
 
         let solver = ClpSolver::from_builder(built);
