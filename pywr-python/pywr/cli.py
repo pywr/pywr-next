c6eac628cd3cbd5485e4722ba7f751da0737776b
James Tomlinson <tomo.bbe@gmail.com>
2026-09-06 16:43:26 +0100

    refactor: Add "runner-service" command to extension module CLI.
    Rather than have its own __main__ in a sub-module. Use the existing
    CLI.

--- pywr-python/pywr/cli.py
+++ pywr-python/pywr/cli.py
@@ -46,5 +46,18 @@ def run(path: str, solver: str, data_path: str | None, output_path: str | None):
     )
 
 
+@cli.command()
+@click.option(
+    "--socket-name",
+    type=str,
+    default="pywr-runner",
+    help="Socket name for the runner service",
+)
+def runner_service(socket_name: str):
+    from ._pywr import run_server
+
+    run_server(socket_name)
+
+
 def start_cli():
     cli()
--- pywr-python/pywr/runner_service.py
+++
@@ -1,4 +0,0 @@
-from ._pywr import run_server
-
-if __name__ == "__main__":
-    run_server("pywr-runner")
