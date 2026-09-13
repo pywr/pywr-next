69bd479ae8e8edf587696f44fa8a878e8478ce88
James Tomlinson <tomo.bbe@gmail.com>
2026-09-13 23:24:03 +0100

    chore(deps): Bump pywr-v1-schema to v0.25.0

--- pywr-schema/src/model.rs
+++ pywr-schema/src/model.rs
@@ -597,7 +597,7 @@ impl ModelSchema {
     ///
     /// See [`ModelSchema::from_v1`] for more information.
     pub fn from_v1_str(v1: &str) -> Result<(Self, Vec<ComponentConversionError>), pywr_v1_schema::PywrSchemaError> {
-        let v1_model: pywr_v1_schema::PywrModel = serde_json::from_str(v1)?;
+        let v1_model = pywr_v1_schema::PywrModel::from_str(v1)?;
 
         Ok(Self::from_v1(v1_model))
     }
